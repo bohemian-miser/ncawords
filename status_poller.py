5f76d3219b5b9c33bcabb5ac01ea79b2796d00f7
bohemian-miser <158785140+bohemian-miser@users.noreply.github.com.>
2026-07-16 14:04:53 +0000

    Fix missing COMP image broken-icon references in UI for queued jobs. TAG=agy

--- status_poller.py
+++ status_poller.py
@@ -2,25 +2,21 @@ import os
 import json
 import time
 
-methods = ['snaps_web_method1', 'snaps_web_method1_noise',
-           'snaps_web_method2', 'snaps_web_method2_noise',
-           'snaps_web_method4', 'snaps_web_method4_noise',
-           'snaps_web_method5', 'snaps_web_method5_noise',
-           'snaps_9_line',      'snaps_9_line_noise',
-           'snaps_cloud',       'snaps_web_evaporate',
-           'snaps_web_hidden',  'snaps_web_evaporate_noise',
-           'snaps_web_hidden_noise']
+import glob
 
 while True:
     status = {}
+    methods = [d for d in glob.glob("snaps_*") if os.path.isdir(d)]
+    
     for d in methods:
-        if os.path.exists(d):
-            files = os.listdir(d)
-            comps = [int(f.split('_')[1].split('.')[0]) for f in files if f.startswith('COMP_')]
-            max_step = max(comps) if comps else -1
-            status[d + '/'] = max_step
-        else:
-            status[d + '/'] = -1
+        files = os.listdir(d)
+        comps = []
+        for f in files:
+            if f.startswith('COMP_') or (f.startswith('TARGET_') and '_' in f):
+                try: comps.append(int(f.split('_')[1].split('.')[0]))
+                except ValueError: pass
+        max_step = max(comps) if comps else -1
+        status[d + '/'] = max_step
             
     with open('status.json', 'w') as f:
         json.dump(status, f)
